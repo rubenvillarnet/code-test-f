c2c7a1f42f608affc39ea200f168798816105eb0
Ruben Villar <ruben@rubenvillar.net>
2021-06-01 09:45:15 +0200

    footer

--- src/components/VerticalLayout/Footer.js
+++ src/components/VerticalLayout/Footer.js
@@ -7,11 +7,8 @@ const Footer = () => {
       <footer className="footer">
         <Container fluid={true}>
           <Row>
-            <Col md={6}>{new Date().getFullYear()} © Skote.</Col>
-            <Col md={6}>
-              <div className="text-sm-end d-none d-sm-block">
-                Design & Develop by Themesbrand
-              </div>
+            <Col md={12} className="text-center">
+              {new Date().getFullYear()} © PÚBLI.CO
             </Col>
           </Row>
         </Container>
